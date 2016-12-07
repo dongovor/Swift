9a28c9f6a265b1dfa347f86ff5cb02b7ed84d111
Dmitry Cherkasov <govor86@gmail.com>
2016-12-07 21:25:27 +0200

    Initial Commit

---
+++ 010-Labels/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  010-Labels
+//
+//  Created by Dmitry Cherkasov on 12/7/16.
+//  Copyright © 2016 Dmitry Cherkasov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
