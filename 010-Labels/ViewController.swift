a018cbb8194179267608572d49cdc5da26a1217c
Dmitry Cherkasov <govor86@gmail.com>
2016-12-08 23:00:48 +0200

    1st commit

--- 010-Labels/ViewController.swift
+++ 010-Labels/ViewController.swift
@@ -10,15 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func theButton(_ sender: Any) {
+        
+        tapCount = tapCount + 1
+        
+        theLabel.text = String(tapCount)
+        
+        if tapCount >= 10 {
+            theLabel.text = "You are the tap master"
+        }
+        
+    }
+    
+    @IBAction func theButton2(_ sender: Any) {
+        
+        theLabel.text = "Buttons are cool!"
+        print("Second button tapped")
+        self.view.backgroundColor = UIColor.brown
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+   
+        
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+   
 
 
 }
